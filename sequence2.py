5456b1e55722314628ea94ab6aa7a55601cab3b3
Rueben-Mason <80366717+Rueben-Mason@users.noreply.github.com>
2021-06-01 12:23:37 +1200

    Add files via upload

---
+++ sequence2.py
@@ -0,0 +1,28 @@
+import turtle
+timmy = turtle.Turtle()
+timmy.pensize(5)
+
+#move to first position
+timmy.penup()
+timmy.goto(10, 60)
+timmy.pendown()
+
+#Draw a purple rectangle , 120 by 50
+timmy.color("purple")
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(30)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(30)
+timmy.color("purple")
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(30)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(30)
